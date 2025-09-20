50df0ed45f929f56700a9a13f8f84acacda2bbe4
Kristvn <joaokristanikk@outlook.com>
2025-09-20 06:17:51 -0300

    Update cars.lua

--- cars.lua
+++ cars.lua
@@ -1,5 +1,5 @@
 local cars = {
-    {name="BNV K4 G", model="BMW M4 Competition G82", internal=nil, rate=nil, price=599, multiplier="1.5x"},
+    {name="BNV K4 G", model="BMW M4 Competition G82", internal="m4 g82", rate=nil, price=599, multiplier="1.5x"},
     {name="Auidy RF6", model="Audi RS6", internal=nil, rate=nil, price=499, multiplier="1.5x"},
     {name="Porx JT3 RF", model="Porsche GT3 RS", internal=nil, rate=nil, price="Offsale (Was 799)", multiplier="1.5x"},
     {name="Mata FX7", model="Mazda RX7", internal=nil, rate=nil, price="Offsale (Was 1099)", multiplier="1x"},
@@ -10,9 +10,9 @@ local cars = {
     {name="Marquis SLX", model="Mercedes SLS", internal=nil, rate="0.05%", price="€180,000–€280,000", multiplier="1.2x"},
     {name="Fia-Te Double", model="Fiat Multipla", internal=nil, rate="0.05%", price="€15,000–€20,000", multiplier="4x"},
     {name="Chule Curgete", model="Corvette C6", internal=nil, rate="0.1%", price="€65,000–€120,000", multiplier="1.2x"},
-    {name="Auidy V7", model="Audi A7", internal=nil, rate="0.1%", price="€50,000–€100,000", multiplier="1.2x"},
+    {name="Auidy V7", model="Audi A7", internal="", rate="0.1%", price="€50,000–€100,000", multiplier="1.2x"},
     {name="Auidy F8", model="Audi R8", internal=nil, rate="0.2%", price="€75,000-∞", multiplier="1.2x"},
-    {name="Toyoda Supwa", model="Toyota Supra", internal=nil, rate="0.3%", price="€75,000-∞", multiplier="1.2x"},
+    {name="Toyoda Supwa", model="Toyota Supra", internal="supra", rate="0.3%", price="€75,000-∞", multiplier="1.2x"},
     {name="BNV K5 e60", model="BMW M5 E60", internal=nil, rate="0.5%", price="€60,000–€90,000", multiplier="1.2x"},
     {name="Auidy RF3", model="Audi RS3", internal="rs3", rate="0.6%", price="€40,000–€55,000", multiplier="1.2x"},
     {name="Missah 750x", model="Nissan 350Z", internal="Nissan 350Z", rate="1%", price="€30,000–€55,000", multiplier="1.2x"},
